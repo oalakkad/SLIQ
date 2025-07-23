e229aeec21416d6fc83efb991ce10f7d358e07df
Malek Dahy <malekdahy8@gmail.com>
2025-07-23 21:19:48 +0300

    Arabic Translation

--- hooks/use-wishlist.ts
+++ hooks/use-wishlist.ts
@@ -7,6 +7,7 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
 export interface WishlistProduct {
   id: number;
   name: string;
+  name_ar: string;
   slug: string;
   image: string;
 }
