90dbbe6f78ba37003dadac4879d7c98c911f4cc1
Malek Dahy <malekdahy8@gmail.com>
2025-09-17 21:11:44 +0300

    Minor fix

--- app/(client)/activation/[uid]/[token]/page.tsx
+++ app/(client)/activation/[uid]/[token]/page.tsx
@@ -1,20 +1,21 @@
 "use client";
 import { useEffect } from "react";
-import { useRouter, useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useActivationMutation } from "@/redux/features/authApiSlice";
 import { Flex, Heading, Spinner, useToast } from "@chakra-ui/react";
 
-export default function Page() {
+export default function Page({
+  params,
+}: {
+  params: { uid: string; token: string };
+}) {
   const router = useRouter();
-  const params = useParams();
   const [activation] = useActivationMutation();
-
   const toast = useToast();
 
   useEffect(() => {
-    // Extract uid and token from params
-    const uid = params?.uid as string;
-    const token = params?.token as string;
+    const uid = params.uid;
+    const token = params.token;
 
     if (!uid || !token) return;
 
@@ -39,12 +40,12 @@ export default function Page() {
       .finally(() => {
         router.push("/auth/login");
       });
-  }, [activation, params, router]);
+  }, [activation, params, router, toast]);
 
   return (
-    <Flex minH={"50vh"} alignItems={"center"} flexDir={"column"}>
-      <Heading mb={"2rem"}>Activating Your Account</Heading>
-      <Spinner color="brand.pink" size={"xl"} />
+    <Flex minH="50vh" alignItems="center" flexDir="column">
+      <Heading mb="2rem">Activating Your Account</Heading>
+      <Spinner color="brand.pink" size="xl" />
     </Flex>
   );
 }
